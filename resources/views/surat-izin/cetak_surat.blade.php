26407fc00966ccc89b0482eff6466de865175e71
Alfin Aswar <alfin7aswar@gmail.com>
2025-08-08 11:14:48 +0700

    123213

--- resources/views/surat-izin/cetak_surat.blade.php
+++ resources/views/surat-izin/cetak_surat.blade.php
@@ -191,15 +191,14 @@
                         <td></td>
                         <td></td>
                     </tr>
+
+                </table>
+                <table>
                     <tr>
                         <td>8.</td>
                         <td>Jumlah keluarga yang menjadi tanggungan</td>
                         <td>:</td>
                         <td>{{ $data->JumlahTanggungan }} orang</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
                     </tr>
                 </table>
             </div>
@@ -323,7 +322,7 @@
                         </div>
                     </td>
                     <!-- Kolom Kanan: Tanda Tangan Panglima -->
-                    <td style="vertical-align:top; width:50%; min-width:180px; text-align:center; padding:4px;">
+                    <td style="vertical-align:top; width:50%; min-width:180px; text-align:left; padding:4px;">
                         <div class="date-location" style="margin-bottom:1px; font-size:12px; line-height:1.1;">
                             Dikeluarkan di Jakarta
                         </div>
@@ -352,12 +351,14 @@
                             tanggal
                             {{ $tanggalIndonesia }}
                         </div>
-                        <div style="font-size:12px; font-weight:bold; margin-bottom:1px; line-height:1.1;">
+                        <hr>
+                        <div style="font-size:12px; margin-bottom:1px; line-height:1.1; text-align:center;">
                             a.n. Panglima Kodam Jaya/Jayakarta<br>
                             Asisten Logistik,
                         </div>
                         <div class="signature-box" style="height: 40px; margin-bottom: 6px;"></div>
-                        <div class="rank-number" style="margin-top:1px; font-size:12px; line-height:1.1;">
+                        <div class="rank-number"
+                            style="margin-top:1px; font-size:12px; line-height:1.1; text-align:center;">
                             Didid Yusnadi, M.Si<br>Kolonel Czi NRP 11000050710979
                         </div>
                     </td>
