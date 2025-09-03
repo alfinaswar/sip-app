4a3328d11e16235cc0fb332a1be86b58a71b0bef
Alfin Aswar <alfin7aswar@gmail.com>
2025-09-03 15:05:46 +0700

    123

---
+++ database/migrations/2025_09_03_080530_add_field_riwayat.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('riwayat_updates', function (Blueprint $table) {
+            $table->string('NamaTtd')->nullable()->after('Tembusan');
+            $table->string('NamaTtd2')->nullable()->after('NamaTtd');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('riwayat_updates', function (Blueprint $table) {
+            //
+        });
+    }
+};
